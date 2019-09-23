5d636be986cc50ea21dce9cacb488e5847e15343
Kensixu <kensixu@hotmail.com>
2019-09-23 17:45:37 -0400

    Check question.

--- magic_eight.py
+++ magic_eight.py
@@ -2,3 +2,13 @@
 def my_function():
     question = input("What is your question?")
     return question
+    
+while True:
+    quest = my_function()
+    if quest[-1] == "?":
+        print("I’msorry, I can only answer questions.")
+        break
+    elif quest == "quit":
+        break
+    else:
+        print("I'm sorry, I can only answer questions.")
